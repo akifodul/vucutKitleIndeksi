f66d6321d927255833c4d44a4d435447e90ae584
[akif ödül] <[akifodul@hotmail.com]>
2023-04-27 06:57:26 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double height, weight, bodyMassIndex;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen Boyunuzu(metre cinsinden) Girin: ");
+        height = input.nextDouble();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Lütfen Kilonuzu Girin: ");
+        weight = input.nextDouble();
+
+        bodyMassIndex = weight/(height*height);
+
+        System.out.println("Vücut Kitle İndeksiniz: " + bodyMassIndex);
+
     }
 }
